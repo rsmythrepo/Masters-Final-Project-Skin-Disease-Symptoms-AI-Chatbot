209a69500399fb52cb9a4a2b6189adaebab139d0
RaphaelleSmyth <raphaelle.smyth@lfs-advisory.com>
2024-02-21 16:52:10 +0100

    preprocessing notes

--- project_code/processing/pre_processing.py
+++ project_code/processing/pre_processing.py
@@ -1,3 +1,6 @@
 '''This processing script takes the images from raw/ s3 bucket folder,
-resizes the images (32,32) and stores them
-in an s3 bucket folder by class=??/ '''
+
+1. resizes the images (32,32) and stores them,
+2. Normalize the images /255.0,
+3. Handle missing images cleaned_images = [img for img in images if img is not None],
+4. in an s3 bucket folder by class=??/ '''
